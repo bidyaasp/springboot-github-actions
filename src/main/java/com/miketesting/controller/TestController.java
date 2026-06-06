8772152a1a703cfc974c0d9119eb13ce9aa87635
Bidyadhar Tripathy <bidyaasp@gmail.com>
2026-06-06 17:23:42 +0530

    code changes

---
+++ src/main/java/com/miketesting/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.miketesting.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, Spring Boot!";
+    }
+
+    @GetMapping("/greet/{name}")
+    public String greet(@PathVariable String name) {
+        return "Hello, " + name;
+    }
+}
---
+++ src/test/java/com/miketesting/SpringbootGithubCicdActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miketesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGithubCicdActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
